e5f2730bff2387befad40266f982657110e092e9
niuwenxiao <3502328021@qq.com>
2018-09-18 20:44:40 +0800

    全局任务三

---
+++ 03-console/01-format.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const user={
+  name:'牛文潇',
+  age:20,
+  qq:'1234556666'
+};
+console.log('name:%s',user.name);
+console.log('age:%d',user.age);
+console.log('JSON:%j',user);
+console.log('qq:%s',user.qq);
+console.log('qq:'+user.qq);
+console.error('Error!something wrong!');
